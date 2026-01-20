d57baf45d91e20092afacea1e97f31184d461bfb
Varvara Efremova <varvara@echus.co>
2026-01-20 13:34:35 +1100

    Add multiarch builds to build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build -t datastudioapp/python-run-base:v0 -t datastudioapp/python-run-base:v0.2 -t datastudioapp/python-run-base:latest . $@
+docker build --platform linux/amd64,linux/arm64 -t datastudioapp/python-run-base:v0 -t datastudioapp/python-run-base:v0.2 -t datastudioapp/python-run-base:latest . $@
