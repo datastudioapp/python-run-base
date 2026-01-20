4ae1a71e3ef86646e16d3917d6f3fa31ff825ab9
Varvara Efremova <varvara@echus.co>
2026-01-20 12:08:32 +1100

    Update DockerHub org name to datastudioapp

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build -t datakits/python-run-base:v0 -t datakits/python-run-base:v0.2 -t datakits/python-run-base:latest . $@
+docker build -t datastudioapp/python-run-base:v0 -t datastudioapp/python-run-base:v0.2 -t datastudioapp/python-run-base:latest . $@
